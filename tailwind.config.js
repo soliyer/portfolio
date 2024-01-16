557044bf9c2a792e26239eac4878b2bf3e086e7e
Soliyerv <vafaei39@gmail.com>
2024-01-15 23:03:23 -0500

    Tailwind Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import colors from 'tailwindcss/colors';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
@@ -11,10 +12,37 @@ export default {
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        fontFamily: {
+            sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            primary: "Playfair Display",
+            body: "work Sans"
+        },
+        container: {
+            padding: {
+                DEFAULT: "1rem",
+                lg: "3rem"
             },
+        },
+        extend: {
+            colors: {
+                "light-primary": "#FAF1E6",
+                "light-secondary": "#FDFAF6",
+                "light-tail-100": "#A9DED2",
+                "light-tail-500": "#54BAB9",
+                "dark-primary": "#050402",
+                "dark-secondary": "#1C1D24",
+                "dark-tail-100": "#07567D",
+                "dark-tail-500": "#292D42",
+                accent: {
+                    DEFAULT: "#ac6b34",
+                    hover: "#925a2b",
+                },
+                paragraph: "#878e99",
+                blue: colors.blue,
+                indigo: colors.indigo,
+                green: colors.green,
+                red: colors.red
+            }
         },
     },
 
